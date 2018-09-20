bc359aed7a2a20fef0a0f5b37540d7d6a7d3029c
Juha Möttönen <juha.mottonen@mainio.co>
2018-09-20 12:24:48 +0300

    Adds support to fetch from API

--- projects/mainio-survey/src/lib/services/survey.service.ts
+++ projects/mainio-survey/src/lib/services/survey.service.ts
@@ -1,21 +1,34 @@
-import { Injectable } from '@angular/core';
-import { v4 as uuid } from 'uuid';
-import { Observable, of } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { Injectable } from "@angular/core";
+import { v4 as uuid } from "uuid";
+import { Observable, of, Subject } from "rxjs";
+import { HttpClient } from "@angular/common/http";
+
+export interface IPostSurvey {
+  id: any;
+  user_id: number;
+  data: any;
+}
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class SurveyService {
+  public onSendSurvey: Subject<IPostSurvey> = new Subject<IPostSurvey>();
+
   constructor(private http: HttpClient) {}
 
   getSurvey(api: string, id: any): Observable<any> {
-    return this.http.get<string[]>(`${api}/${id}.json`); // REMOVE .JSON IF NECESSARY
+    return this.http.get<string[]>(`${api}/${id}`); // REMOVE .JSON IF NECESSARY
   }
 
   sendSurveyToServer(survey, user_id = 0) {
     // POST TO SERVER HERE
     // ...
-    console.log({ id: uuid(), user_id, ...survey.data });
+    let item: IPostSurvey = {
+      id: uuid(),
+      user_id: user_id,
+      data: survey.data
+    };
+    this.onSendSurvey.next(item);
   }
 }
