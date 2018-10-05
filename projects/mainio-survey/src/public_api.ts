ad90133ce52a8d4b6dbdfbe22cb691894ee23f59
Juha Möttönen <juha.mottonen@mainio.co>
2018-10-05 15:51:51 +0300

    Adds survey service as public from libbrary

--- projects/mainio-survey/src/public_api.ts
+++ projects/mainio-survey/src/public_api.ts
@@ -2,4 +2,5 @@
  * Public API Surface of mainio-survey
  */
 
-export * from './lib/mainio-survey.module';
+export * from "./lib/mainio-survey.module";
+export * from "./lib/services/survey.service";
